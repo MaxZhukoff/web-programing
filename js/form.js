f067c6138c5387dbab74b56e5c9a04992459f707
MaxZhukoff <62457503+MaxZhukoff@users.noreply.github.com>
2021-11-23 17:32:14 +0300

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,27 @@
+showAppointment()
+function showAppointment() {
+    let commentField = document.getElementById('comment-field');
+    let out = '';
+    if (localStorage.getItem("appoint") !== null) {
+        let appointments = JSON.parse(localStorage.getItem('appoint'))
+        out += `<p class="alert alert-primary" role="alert">Имя: ${appointments.first_name}</p>`;
+        out += `<p class="alert alert-primary" role="alert">Фамилия: ${appointments.surname}</p>`;
+        out += `<p class="alert alert-primary" role="alert">Отчество: ${appointments.middle_name}</p>`;
+        out += `<p class="alert alert-primary" role="alert">Номер телефона: ${appointments.phone_number}</p>`;
+        out += `<p class="alert alert-primary" role="alert">Время записи: ${appointments.date}</p>`;
+        out += `<p class="alert alert-primary" role="alert">Врач: ${appointments.doctor}</p>`;
+        commentField.innerHTML = out;
+    }
+}
+
+document.getElementById("comment-add").addEventListener('click', function(event) {
+    let appointment = {
+        first_name: document.getElementById('first_name').value,
+        surname: document.getElementById('surname').value,
+        middle_name: document.getElementById('middle_name').value,
+        phone_number: document.getElementById('phone_number').value,
+        date: document.getElementById('date1').value,
+        doctor: document.getElementById('doctor_sel').value
+    }
+    localStorage.setItem('appoint', JSON.stringify(appointment))
+});
--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let loadTime = (function () {
     let now = new Date().getTime();
-    let page_load_time = now - performance.timing.navigationStart;
-    return page_load_time / 1000;
+    let page_load_time = now - performance.timeOrigin
+    let time_load = page_load_time / 1000;
+    return time_load.toFixed(4);
 })();
